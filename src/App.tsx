72e6ae8a3b1386500c4d4523f242454700faab1e
Ryan Tan <tanyuryan@gmail.com>
2025-04-06 00:24:32 +0800

    Integrate react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, createTheme, Stack, ThemeProvider, Typography } from '@mui/material'
+import { Container, Stack, Typography } from '@mui/material'
 import Users from './Users'
 import useBill from './hooks/use-bill'
 import Items from './Items'
@@ -6,52 +6,25 @@ import Taxes from './Taxes'
 import PriceSummary from './PriceSummary'
 import WhoPaid from './WhoPaid.tsx'
 import Report from './Report.tsx'
-
-const theme = createTheme({
-  typography: {
-    fontSize: 12,
-    fontFamily: 'Roboto',
-    h1: {
-      fontSize: '2rem',
-      fontWeight: 'bold',
-    },
-    h2: {
-      fontSize: '1.5rem',
-      fontWeight: 'bold',
-    },
-    h3: {
-      fontSize: '1.3rem',
-      fontWeight: 'bold',
-    },
-    h4: {
-      fontSize: '1.1rem',
-      fontWeight: 'bold',
-    },
-    h5: {
-      fontSize: '1rem',
-      fontWeight: 'bold',
-    },
-  },
-})
+import { useParams } from 'react-router'
 
 function App() {
+  const { slug } = useParams()
   const { data, methods } = useBill()
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth='sm'>
-        <Stack spacing={1}>
-          <Typography variant='h1'>Bill Splitter</Typography>
-          <Users data={data} methods={methods} />
-          <Items data={data} methods={methods} />
-          <Taxes data={data} methods={methods} />
-          <PriceSummary data={data} methods={methods} />
-          <WhoPaid data={data} methods={methods} />
-          <Report data={data} methods={methods} />
-          {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-        </Stack>
-      </Container>
-    </ThemeProvider>
+    <Container maxWidth='sm'>
+      <Stack spacing={1}>
+        <Typography variant='h1'>Bill Splitter</Typography>
+        <Users data={data} methods={methods} />
+        <Items data={data} methods={methods} />
+        <Taxes data={data} methods={methods} />
+        <PriceSummary data={data} methods={methods} />
+        <WhoPaid data={data} methods={methods} />
+        <Report data={data} methods={methods} />
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      </Stack>
+    </Container>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,47 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  typography: {
+    fontSize: 12,
+    fontFamily: 'Roboto',
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 'bold',
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+    },
+    h3: {
+      fontSize: '1.3rem',
+      fontWeight: 'bold',
+    },
+    h4: {
+      fontSize: '1.1rem',
+      fontWeight: 'bold',
+    },
+    h5: {
+      fontSize: '1rem',
+      fontWeight: 'bold',
+    },
+  },
+})
+
+const PROG = 'bill-splitter-v2'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`${PROG}`} element={<App />} />
+          <Route path={`${PROG}/s/:slug`} element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 )
